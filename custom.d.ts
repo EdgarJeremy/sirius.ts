787eb8bff26c44141c739a627537976441f6a9c7
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-04-28 22:09:35 +0800

    (*) remove unused import

--- custom.d.ts
+++ custom.d.ts
@@ -1,6 +1,5 @@
 import { Request } from 'express';
 import { UserInstance } from './src/models/User';
-import { ParsedQuery } from './src/middlewares/pipes/parser';
 
 declare global {
     namespace Express {
