14807e307473cd88cdfcebd80aff744c1c5979a2
EdgarJeremy <edgarpontoh3141@gmail.com>
2019-11-28 23:42:54 +0800

    (^) update query parser using recursive property generator

--- src/helpers/Parser.ts
+++ src/helpers/Parser.ts
@@ -5,12 +5,15 @@ import wrapValidation from '../middlewares/validation/request';
 import { checkSchema } from 'express-validator/check';
 
 export interface ICollectionOptions {
+	distinct?: boolean;
 	limit?: number;
 	offset?: number;
 	attributes?: string[];
+	as?: string;
 	include?: ICollectionIncludeOptions[];
 	order?: string[] | string[][];
-	where?: any
+	where?: any;
+	group?: string | string[];
 }
 
 interface ICollectionIncludeOptions extends ICollectionOptions {
@@ -19,23 +22,54 @@ interface ICollectionIncludeOptions extends ICollectionOptions {
 
 export class Parser {
 	public static parseQuery<T>(q: string, models: ModelFactoryInterface): sequelize.FindOptions<T> {
-		const raw: ICollectionOptions = JSON.parse(new Buffer(q, 'base64').toString());
+		const raw: ICollectionOptions = JSON.parse(Buffer.from(q, 'base64').toString());
 		const parsed: sequelize.FindOptions<T> = {
-			attributes: raw.attributes,
-			include: raw.include
-				? raw.include.map((m: ICollectionIncludeOptions) => ({
-						model: models[m.model],
-						attributes: m.attributes ? m.attributes : ['id'],
-						where: m.where
-				  }))
-				: [],
+			distinct: raw.distinct,
+			attributes: this.buildAttributes(raw.attributes),
 			limit: raw.limit,
 			offset: raw.offset,
 			order: raw.order,
-			where: raw.where
+			where: raw.where,
+			include: this.buildIncludes(raw.include, models),
+			group: raw.group,
+			// @ts-ignore
+			as: raw.as
 		};
 
 		return parsed;
+	}
+
+	private static buildIncludes(
+		include: ICollectionIncludeOptions[] | undefined,
+		models: ModelFactoryInterface,
+	): (sequelize.Model<any, any> | sequelize.IncludeOptions)[] {
+		return include
+			? include.map((m: ICollectionIncludeOptions) => ({
+					model: models[m.model],
+					attributes: this.buildAttributes(m.attributes),
+					where: m.where,
+					limit: m.limit,
+					offset: m.offset,
+					order: m.order,
+					include: this.buildIncludes(m.include, models),
+					as: m.as
+			  }))
+			: [];
+	}
+
+	private static buildAttributes(
+		rawAttributes: ICollectionOptions['attributes'],
+	): sequelize.FindOptionsAttributesArray {
+		return rawAttributes
+			? rawAttributes.map((f: string) => {
+					const o: string[] = f.split(':');
+					if (o.length > 1) {
+						return sequelize.fn(o[0], o[1]);
+					} else {
+						return f;
+					}
+			  })
+			: [];
 	}
 
 	public static validateQ(): express.Handler[] {
@@ -46,7 +80,7 @@ export class Parser {
 					custom: {
 						options: (value: string): boolean => {
 							try {
-								const raw: any = JSON.parse(new Buffer(value, 'base64').toString());
+								const raw: any = JSON.parse(Buffer.from(value, 'base64').toString());
 								return true;
 							} catch (e) {
 								return false;
